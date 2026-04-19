66063ad8985cc4abee720b2a9d3a98711d35fa4d
svizcaino26 <svizcaino26@gmail.com>
2026-04-19 11:52:07 -0500

    testing new implementations

--- app/src/main/java/com/ohmystore/App.java
+++ app/src/main/java/com/ohmystore/App.java
@@ -1,12 +1,11 @@
 package com.ohmystore;
 
-import com.ohmystore.model.Product;
 import com.ohmystore.config.Database;
+import com.ohmystore.model.Product;
 import com.ohmystore.repository.ProductRepository;
-import com.ohmystore.dto.NewProductRequest;
 import io.github.cdimascio.dotenv.Dotenv;
-
 import java.sql.SQLException;
+import java.util.List;
 
 public class App {
   public static void main(String[] args) {
@@ -20,15 +19,28 @@ public class App {
     Database db = new Database(url, user, password);
     ProductRepository productRepo = new ProductRepository(db);
 
-    NewProductRequest req = new NewProductRequest("SLR Cammera", 60000);
+    // NewProductRequest req = new NewProductRequest("SLR Cammera", 60000);
 
+    // try {
+    //   req.validate();
+    //   Product newProduct = productRepo.save(req);
+    //   System.out.println(newProduct);
+    // } catch (SQLException e) {
+    //   System.err.println(e);
+    // } catch (IllegalArgumentException e) {
+    //   System.err.println(e);
+    // }
     try {
-      req.validate();
-      Product newProduct = productRepo.save(req);
-      System.out.println(newProduct);
+      List<Product> products = productRepo.findAll();
+      for (Product p : products) {
+        System.out.println(p);
+      }
+      System.out.println();
+      List<Product> productsLimit = productRepo.findAllLimited(2);
+      for (Product p : productsLimit) {
+        System.out.println(p);
+      }
     } catch (SQLException e) {
-      System.err.println(e);
-    } catch (IllegalArgumentException e) {
       System.err.println(e);
     }
   }
